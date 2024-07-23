ecf67a4d0638f2d64788fbc013ad69cad517d0cd
Tracy <tracyyyshrestha@gmail.com>
2024-07-23 11:07:36 +0545

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Product/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Product from './components/Product.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Product
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
